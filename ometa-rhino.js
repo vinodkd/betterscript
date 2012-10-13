17346e2aaae264d5f8f72bdd2398ea16baa056ab
vinodkd <vinod@vinodkd.org>
2012-10-13 19:55:03 +0530

    added debug function that kinda works for the most part

--- ometa-rhino.js
+++ ometa-rhino.js
@@ -6,12 +6,37 @@ load("/Users/vinodkd/installed/ometa-js/bs-ometa-compiler.js")
 load("/Users/vinodkd/installed/ometa-js/bs-ometa-optimizer.js")
 load("/Users/vinodkd/installed/ometa-js/bs-ometa-js-compiler.js")
 
+matchFailed = function (grammar, errorPos) {
+  var lines = grammar.input.lst.split('\n');
+  var pos = 0, l = 0;
+  var msg = ["Execution error: input matching failed at position: " + errorPos];
+
+  while (pos < errorPos) {
+    var line = lines[l], length = line.length;
+    if (pos + length >= errorPos) {
+      var c = errorPos - pos;
+      msg.push("  line:" + (l + 1) + ", column:" + c);
+      msg.push(line)
+      // replicate str n times
+      function replicate(str, n) {
+        if (n < 1) return "";
+        var t = [];
+        for (var i=0; i<n; i++) t.push(str);
+        return t.join('');
+      }
+      msg.push(replicate('-', c) + '^');
+    }
+    pos += length + 1;
+    l++;
+  }
+  alert(msg.join('\n'));
+}
+
 alert = print
 
 translateCode = function(s) {
   var translationError = function(m, i) { alert("Translation error - please tell Alex about this!"); throw fail },
-      tree             = BSOMetaJSParser.matchAll(s, "topLevel", undefined, function(m, i) {
-                                                                              throw objectThatDelegatesTo(fail, {errorPos: i}) })
+      tree             = BSOMetaJSParser.matchAll(s, "topLevel", undefined, matchFailed)
   return BSOMetaJSTranslator.match(tree, "trans", undefined, translationError)
 }
 var grammar = readFile(arguments[0]);
